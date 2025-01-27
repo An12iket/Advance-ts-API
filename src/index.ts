ffd10ccc420434fb37a85f2f84ee85c147673668
an12iket <aniketchavan121012@gmail.com>
2025-01-27 22:21:25 +0530

    Record and map()

--- src/index.ts
+++ src/index.ts
@@ -1,44 +1,61 @@
-interface User {
-    id:string,
-    name: string,
-    age:number,
-    email:string,
-    password:string
-};
+// interface User {
+//     id:string,
+//     name: string,
+//     age:number,
+//     email:string,
+//     password:string
+// };
 
-// // using Pick<> here...
-type UpdatesProps = Pick<User, "name" | "age" | "email">
+// // // using Pick<> here...
+// type UpdatesProps = Pick<User, "name" | "age" | "email">
 
-function updateUser(updatesProps:UpdatesProps){
-    // hit the database to update the user
+// function updateUser(updatesProps:UpdatesProps){
+//     // hit the database to update the user
+// }
+
+// // using Partial<> here...
+// interface User {
+//     id:string,
+//     name: string,
+//     age:number,
+//     email:string,
+//     password:string
+// };
+
+// type UpdatePropsOptional = Partial<UpdatesProps>
+
+// function updateUser(updatesProps:UpdatePropsOptional){
+//     // hit the database to update the user
+// }
+// updateUser({
+//     name:"Aniket"
+// })
+
+// // using readonly here
+// type User = {
+//     name: string,
+//     age: number,
+// }
+
+// const user: Readonly<User> = {
+//     name:"John",
+//     age: 21
+// }
+// user.age = "asldkl"
+
+
+// record here...
+type Users = Record<string, { age: number; name: string }>;
+
+const users: Users = {
+    "ashdh@1": { age:22, name:"fuefuweh" },
+    "ashdh@2": { age:21, name:"ityuiuyi" }
 }
 
-// using Partial<> here...
-interface User {
-    id:string,
-    name: string,
-    age:number,
-    email:string,
-    password:string
-};
+//also map()
+const usersInfo = new Map();
+usersInfo.set("jhfheh@", { name: "shd", age:21, email:"sdhkjsdh@" })
+usersInfo.set("sdsd@", { name: "fggfg", age:24, email:"fdffrrt@" })
 
-type UpdatePropsOptional = Partial<UpdatesProps>
-
-function updateUser(updatesProps:UpdatePropsOptional){
-    // hit the database to update the user
-}
-updateUser({
-    name:"Aniket"
-})
-
-// using readonly here
-type User = {
-    name: string,
-    age: number,
-}
-
-const user: Readonly<User> = {
-    name:"John",
-    age: 21
-}
-user.age = "asldkl"
+const user = usersInfo.get("jhfheh@")
+usersInfo.delete("sdsd@")
