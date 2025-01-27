3196033a7aac0d55f64e37528adf1907e55b86ea
an12iket <aniketchavan121012@gmail.com>
2025-01-27 22:22:29 +0530

    Record and Map()

--- src/index.ts
+++ src/index.ts
@@ -1,50 +1,50 @@
-// interface User {
-//     id:string,
-//     name: string,
-//     age:number,
-//     email:string,
-//     password:string
-// };
+interface User {
+    id:string,
+    name: string,
+    age:number,
+    email:string,
+    password:string
+};
 
-// // // using Pick<> here...
-// type UpdatesProps = Pick<User, "name" | "age" | "email">
+// // using Pick<> here...
+type UpdatesProps = Pick<User, "name" | "age" | "email">
 
-// function updateUser(updatesProps:UpdatesProps){
-//     // hit the database to update the user
-// }
+function updateUser(updatesProps:UpdatesProps){
+    // hit the database to update the user
+}
 
-// // using Partial<> here...
-// interface User {
-//     id:string,
-//     name: string,
-//     age:number,
-//     email:string,
-//     password:string
-// };
+// using Partial<> here...
+interface User {
+    id:string,
+    name: string,
+    age:number,
+    email:string,
+    password:string
+};
 
-// type UpdatePropsOptional = Partial<UpdatesProps>
+type UpdatePropsOptional = Partial<UpdatesProps>
 
-// function updateUser(updatesProps:UpdatePropsOptional){
-//     // hit the database to update the user
-// }
-// updateUser({
-//     name:"Aniket"
-// })
+function updateUser(updatesProps:UpdatePropsOptional){
+    // hit the database to update the user
+}
+updateUser({
+    name:"Aniket"
+})
 
-// // using readonly here
-// type User = {
-//     name: string,
-//     age: number,
-// }
+// using readonly here
+type User = {
+    name: string,
+    age: number,
+}
 
-// const user: Readonly<User> = {
-//     name:"John",
-//     age: 21
-// }
-// user.age = "asldkl"
+const user: Readonly<User> = {
+    name:"John",
+    age: 21
+}
+user.age = "asldkl"
 
 
-// record here...
+// using record here...
 type Users = Record<string, { age: number; name: string }>;
 
 const users: Users = {
